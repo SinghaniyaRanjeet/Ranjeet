307b3ba08951286b4b90ea3ccbc9785f018263d8
SinghaniyaRanjeet <singhaniyakuma1234@gmail.com>
2019-06-15 15:46:02 +0530

    Work

--- app/src/main/java/com/example/e_commerce/LoginActivity.java
+++ app/src/main/java/com/example/e_commerce/LoginActivity.java
@@ -2,8 +2,8 @@ package com.example.e_commerce;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,9 +12,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
     private EditText edt_Email, edt_Password;
     private TextView txt_Forget;
-    private Button btn_login, btn_signUp;
+
 
 
     @Override
@@ -29,20 +30,36 @@ public class LoginActivity extends AppCompatActivity {
 
     public void onClick(View view) {
         //login
+        Log.e(TAG, "ON click Method " + view.getId());
         switch (view.getId()) {
             case R.id.login_btn:
-                forword_register();
+
                 Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show();
                 return;
             case R.id.signUp_btn:
+                forword_register();
                 Toast.makeText(this, "signUp", Toast.LENGTH_SHORT).show();
                 return;
         }
     }
 
     private void forword_register() {
+        Log.e(TAG, "Forword Register Activity" + getIntent());
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+    }
+
+    private void logintext() {
+        Log.e(TAG, "Email Id Get" + edt_Email.getId());
+        Log.e(TAG, "Password Get" + edt_Password.getId());
+        String email = edt_Email.getText().toString();
+        String pass = edt_Password.getText().toString();
+
+        if (email.equals("")) {
+            edt_Email.setError("Please Enter Email");
+        } else if (pass.equals("")) {
+            edt_Password.setError("Please Enter Password");
+        }
     }
 }
