fc334332e133e8f6243389952f3cd18a0ed2001d
SinghaniyaRanjeet <singhaniyakuma1234@gmail.com>
2019-06-15 13:27:50 +0530

    register

--- app/src/main/java/com/example/e_commerce/LoginActivity.java
+++ app/src/main/java/com/example/e_commerce/LoginActivity.java
@@ -1,22 +1,47 @@
 package com.example.e_commerce;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText editText;
+    private EditText edt_Email, edt_Password;
+    private TextView txt_Forget;
+    private Button btn_login, btn_signUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edt_Email = findViewById(R.id.email_edt);
+        edt_Password = findViewById(R.id.password_edt);
+        txt_Forget = findViewById(R.id.forget_txt);
+
     }
 
     public void onClick(View view) {
         //login
+        switch (view.getId()) {
+            case R.id.login_btn:
+                forword_register();
+                Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show();
+                return;
+            case R.id.signUp_btn:
+                Toast.makeText(this, "signUp", Toast.LENGTH_SHORT).show();
+                return;
+        }
+    }
+
+    private void forword_register() {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/e_commerce/RegisterActivity.java
+++ app/src/main/java/com/example/e_commerce/RegisterActivity.java
@@ -2,18 +2,55 @@ package com.example.e_commerce;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText userName_edt, adharCard_edt, mobile_edt,
+            email_edt, gst_edt, address_edt, city_edt, panCard_edt, pass_edt;
+    private TextView register_edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //get Component id
+        userName_edt = findViewById(R.id.user_edt);
+        adharCard_edt = findViewById(R.id.adhar_edt);
+        mobile_edt = findViewById(R.id.mobile_edt);
+        email_edt = findViewById(R.id.email_editText);
+        gst_edt = findViewById(R.id.gst_number_edt);
+        address_edt = findViewById(R.id.address_edt);
+        city_edt = findViewById(R.id.city_edt);
+        panCard_edt = findViewById(R.id.pan_edt);
+        pass_edt = findViewById(R.id.password_edt);
+
     }
 
     public void onClick(View view) {
         //Register use button
+        switch (view.getId()) {
+            case R.id.register_txt:
+                Toast.makeText(this, "Register Successfully", Toast.LENGTH_SHORT).show();
+                return;
+        }
     }
+
+    private void getdataEdit() {
+        String user_name = userName_edt.getText().toString();
+        String adhar_card = adharCard_edt.getText().toString();
+        String mobile_number = mobile_edt.getText().toString();
+        String email_id = email_edt.getText().toString();
+        String gst_number = gst_edt.getText().toString();
+        String address = address_edt.getText().toString();
+        String city_name = city_edt.getText().toString();
+        String pan_card = panCard_edt.getText().toString();
+        String password = pass_edt.getText().toString();
+
+
+    }
+
 }
