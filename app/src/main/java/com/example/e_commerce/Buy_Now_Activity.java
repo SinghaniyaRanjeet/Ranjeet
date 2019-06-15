9cad6e44668f37fe663c6f2d55869ad1ded4ec47
adhyabiz <creationadhya@gmail.com>
2019-06-15 18:59:24 +0530

    aman singh cart

---
+++ app/src/main/java/com/example/e_commerce/Buy_Now_Activity.java
@@ -0,0 +1,14 @@
+package com.example.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Buy_Now_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy__now_);
+    }
+}
--- app/src/main/java/com/example/e_commerce/Main3Activity.java
+++ app/src/main/java/com/example/e_commerce/Main3Activity.java
@@ -2,13 +2,30 @@ package com.example.e_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
+
+
+    private Button TextView_BuyNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        TextView_BuyNow=findViewById(R.id.TextView_BuyNow);
+
+        TextView_BuyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Main3Activity.this,Buy_Now_Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
